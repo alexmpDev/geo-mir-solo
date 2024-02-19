c050e1f00bfd182b3fe20ce1eecd6f8388e32d8d
«alexmpDev» <alexmpDev@gmail.com>
2024-02-19 18:48:03 +0100

    End places

--- laravel/tests/Feature/PlaceTest.php
+++ laravel/tests/Feature/PlaceTest.php
@@ -118,7 +118,7 @@ class PlaceTest extends TestCase
     {
         Sanctum::actingAs(new User(self::$testUser));
         // Cridar servei web de l'API
-        $response = $this->postJson("/api/places/show", self::$validData);
+        $response = $this->postJson("/api/places/show/1");
         // Revisar que no hi ha errors de validació
         
         $response->assertJsonFragment(['success' => true]);
